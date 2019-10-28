2b515818f9d897678bda4b3ffd18803b1a6ffda3
Fiona Fay <fifi@calicastle.com>
2019-10-27 22:21:22 -0700

    Contact

--- web110/contact.php
+++ web110/contact.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width" />
   <link rel="stylesheet" href="css/portal.css" />
   <link rel="stylesheet" href="css/nav.css" />
+  <link rel="stylesheet" href="css/forms.css" />
  </head>
  <body>
      <header>
@@ -26,9 +27,27 @@
      </header>
      
    <main class="wrapper">
-       
-       <h2 class="subheader">Contact Fiona</h2>
-       <p>Contact Form assignment, coming soon!</p>
+      <h2 class="subheader">Contact Fiona</h2>
+      <div class="main-content">
+       <?php
+         /*
+          * Below are 2 different forms to be re-used       
+          * 
+          * Only use one at a time, comment out the other!       
+          *
+          */
+
+         include 'includes/contact_include.php'; #site keys & code here
+    
+        $toAddress = "huangritafifi@gamil.com";  //place your/your client's email address  here
+         $toName = "Fiona Fay"; //place your client's name here
+         $website = "Contact For Test";  //place NAME of your client's website
+
+         //echo loadContact('simple.php');#demonstrates a simple contact form
+         echo loadContact('multiple.php');#demonstrates multiple form elements
+
+   ?>
+      </div>
 
      <footer>
       <p><small>&copy; 2019 by <a href="contact.php">Contact Fiona Fay </a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
